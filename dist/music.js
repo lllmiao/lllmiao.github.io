3814507048533f4a0c79a9e7cb5501f85895774f
miao <905360830@qq.com>
2020-07-21 00:32:58 +0800

    Site updated: 2020-07-21 00:32:45

---
+++ dist/music.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    autoplay: true,
+    loop: 'all',
+    audio: [
+{
+        name: 'hotline bling',
+        artist: 'Billie Eilish',
+        url: 'https://m701.music.126.net/20200720010232/20ac2de183e408edcbaff27472e1ada5/jdyyaac/545c/555a/550b/c672e6e369b076f10927ba65bc66eaa1.m4a',
+        cover: 'https://p2.music.126.net/b_3apMti5PmlZvUioDwtDQ==/109951164141864679.jpg?param=177y177',
+    }
+]
+});
